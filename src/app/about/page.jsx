19f7d13c26eb43fe2e494d0ff6442188325788c2
Ömer Gülçiçek <iletisim@omergulcicek.com>
2023-10-10 23:27:53 +0300

    Next.js 13

---
+++ src/app/about/page.jsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return <h1>About Page</h1>
+}
+
+export const metadata = {
+  title: "About Page",
+}
---
+++ src/app/layout.jsx
@@ -0,0 +1,26 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="tr" suppressHydrationWarning>
+      <body suppressHydrationWarning>{children}</body>
+    </html>
+  )
+}
+
+export const metadata = {
+  metadataBase: new URL("https://github.com/omergulcicek/nextjs-boilerplate"),
+  robots: {
+    index: true,
+    follow: true,
+  },
+  title: "Next.js 13 Boilerplate",
+  description: "Next.js 13 Boilerplate",
+  themeColor: "#ffffff",
+  alternates: {
+    canonical: "/",
+  },
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
+}
