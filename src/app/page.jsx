2f33ff0d8cc60544c6e6fe44b02c1bf1c68b5ccb
Ömer Gülçiçek <iletisim@omergulcicek.com>
2023-10-12 00:48:16 +0300

    Absolute Imports and Module Path Aliases

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,13 @@
 import Link from "next/link"
 
+import Button from "@/components/button"
+
 export default function Page() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
       <Link href="/about">Go "About" page</Link>
+      <Button />
     </>
   )
 }
