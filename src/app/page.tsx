210a6067cb444510d1c2545489b8187d5640bf0b
omergulcicek <iletisim@omergulcicek.com>
2024-03-26 15:11:12 +0300

    Update global CSS variables and add Button and Separator components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { Button } from "@/ui/button";
+import { Separator } from "@/ui/separator";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-8">
@@ -12,25 +15,23 @@ export default function Home() {
         loading="lazy"
       />
 
-      <nav>
+      <nav className="flex h-5 items-center space-x-1 text-sm">
         <Link
           href="https://github.com/omergulcicek/"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-blue-600 hover:underline"
         >
-          @omergulcicek
+          <Button variant="link">@omergulcicek</Button>
         </Link>
 
-        <span> / </span>
+        <Separator orientation="vertical" />
 
         <Link
           href="https://github.com/omergulcicek/nextjs-boilerplate/tree/main"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-blue-600 hover:underline"
         >
-          nextjs-boilerplate
+          <Button variant="link">nextjs-boilerplate</Button>
         </Link>
       </nav>
     </main>
