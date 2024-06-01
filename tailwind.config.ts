910ab5312f518202583b4a08963e6d6b5648bd04
omergulcicek <iletisim@omergulcicek.com>
2024-06-02 00:18:41 +0300

    Remove duplicate import statements in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,5 @@
 import type { Config } from "tailwindcss"
 
-import type { Config } from "tailwindcss"
-
-import type { Config } from "tailwindcss"
-
 const config = {
 	darkMode: ["class"],
 	content: [
