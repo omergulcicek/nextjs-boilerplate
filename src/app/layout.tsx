490366b73919520ebe3b24b287e7286bbf3ca8a0
Ömer Gülçiçek <omer.gulcicek@alisgidis.com>
2025-05-11 16:30:18 +0300

    layout.tsx dosyasında ana içerik alanına flex-1 sınıfı eklendi ve body etiketine minimum yükseklik ile flex düzeni uygulandı. Bu değişiklik, sayfanın daha iyi bir görünüm ve düzen sağlamasına yardımcı olacak.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,12 +30,12 @@ export default function RootLayout({
 	return (
 		<html lang="en" className="light" style={{ colorScheme: "light" }}>
 			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+				className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
 				suppressHydrationWarning
 			>
 				<Providers>
 					<Header />
-					<main>{children}</main>
+					<main className="flex-1">{children}</main>
 					<Footer />
 				</Providers>
 			</body>
