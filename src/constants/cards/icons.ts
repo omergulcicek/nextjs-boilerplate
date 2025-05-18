8def584e8fd6c0321edf4f2e5e3487c4f9b9f217
Ömer Gülçiçek <omer.gulcicek@alisgidis.com>
2025-05-18 16:56:41 +0300

    Kredi kartı ikonları ve regex tanımları güncellendi. Mastercard ve Visa ikonları yer değiştirirken, Masterpass için yeni bir regex deseni eklendi.

--- src/constants/cards/icons.ts
+++ src/constants/cards/icons.ts
@@ -1,8 +1,8 @@
 import { Mastercard, Visa } from "@/components/icons"
 
 export const CARD_ICONS = {
-	VISA: Mastercard,
-	MASTERCARD: Visa
+	MASTERPASS: Mastercard,
+	VISA: Visa
 } as const
 
 export type CardIcons = keyof typeof CARD_ICONS
