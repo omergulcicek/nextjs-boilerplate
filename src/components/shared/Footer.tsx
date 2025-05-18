f2cff4182432ff1ceeaf29e830eb4827abd89198
Ömer Gülçiçek <omer.gulcicek@alisgidis.com>
2025-05-19 01:28:11 +0300

    Footer bileşenine yeni bir bağlantı eklendi; Ömer Gülçiçek'in web sitesine yönlendiren bir buton oluşturuldu. Ayrıca, bileşenin düzeni güncellendi.

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,9 +1,21 @@
+import Link from "next/link"
+
+import { Button } from "@/ui"
+
 export default function Footer() {
 	return (
 		<footer className="border-t">
 			<div className="container">
 				<div className="flex justify-between items-center py-4 font-mono text-sm">
 					<p>© Copyright {new Date().getFullYear()} Next.js Boilerplate</p>
+
+					<Link
+						href="https://omergulcicek.com"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<Button variant="link">Ömer Gülçiçek</Button>
+					</Link>
 				</div>
 			</div>
 		</footer>
