f0a0f805f8a45a3ba503daac4b4e71a837c647da
Ömer Gülçiçek <omer.gulcicek@alisgidis.com>
2025-06-04 01:13:22 +0300

    Update Home component to align items to the start in the layout.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import { Appearance, FormExample, Hero, QueryExample, Stack } from "@/widgets"
 
 export default function Home() {
 	return (
-		<div className="container flex flex-col gap-2 lg:p-20">
+		<div className="container flex flex-col gap-2 items-start lg:p-20">
 			<Hero />
 			<Stack />
 			<div className="grid grid-cols-1 gap-10 lg:grid-cols-2 xl:grid-cols-3">
