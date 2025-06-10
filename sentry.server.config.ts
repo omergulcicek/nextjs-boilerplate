905eb45a9b16a5236b3d05abb725008b99899325
Ömer Gülçiçek <omer.gulcicek@alisgidis.com>
2025-06-11 01:11:48 +0300

    Add Sentry integration and configuration for client and server. Update environment variables and package dependencies.

---
+++ sentry.server.config.ts
@@ -0,0 +1,17 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs"
+
+import { APP_CONFIG } from "@/constants"
+
+Sentry.init({
+	dsn: APP_CONFIG.SENTRY_DSN,
+
+	// Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+	tracesSampleRate: 1,
+
+	// Setting this option to true will print useful information to the console while you're setting up Sentry.
+	debug: false
+})
