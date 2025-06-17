f23076592f573c4af52423b32778e8e26f06a93b
Ömer Gülçiçek <omer.gulcicek@alisgidis.com>
2025-06-17 23:30:55 +0300

    Update Storybook preview configuration to include Next.js app directory support.

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -10,12 +10,11 @@ const preview: Preview = {
 				date: /Date$/i
 			}
 		},
-
 		a11y: {
-			// 'todo' - show a11y violations in the test UI only
-			// 'error' - fail CI on a11y violations
-			// 'off' - skip a11y checks entirely
 			test: "todo"
+		},
+		nextjs: {
+			appDirectory: true
 		}
 	}
 }
